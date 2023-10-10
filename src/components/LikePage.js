32a552c553a23c9c9491f4f8e640ff161b585d89
skolgit <em8630gr-s@student.lu.se>
2023-10-10 20:54:19 +0200

    Update LikePage.js
    inefficiently changes photos

--- src/components/LikePage.js
+++ src/components/LikePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { useData } from "../dataContext";
+import { fetchAnimalTypes, fetchAnimals } from "../petfinder";
 
 // TODO
 // Get several pets at the same time, add to a list, add/remove from list on like/dislike (makes 'swiping' instantaneous and api calls happen in the background)
@@ -8,20 +9,22 @@ import { useData } from "../dataContext";
 
 // Save a list of all liked/disliked pets, make sure the same pet isnt showed again
 
+// Many pets dont have photos, add stuff do deal with this
 
 
 function LikePage() {
   // eslint-disable-next-line no-unused-vars
   const { settings, setSettings, matches, setMatches } = useData();
-  const [pet, setPet] = useState({petName: "Pet Name", petImg: "cat1.jpg", petDetails: "Details about pet"})
+  const [petImg, setPetImg] = useState(""); 
 
-  //tmp
-  const imgs = ["cat1.jpg", "cat2.webp", "dog1.jpg", "dog2.webp"]
+  const [currentPage, setCurrentPage] = useState(1) // Needs improvement
 
-  function handleLikeClick() {
-    const pet = getNewPet()
-    setPet(pet)
+  const petPref = settings.petPreference ? settings.petPreference : "Dog";
+
+
+  async function handleLikeClick() {
     addToMatches()
+    const pet = getNewPet()
   }
 
   function handleDislikeClick() {
@@ -32,12 +35,38 @@ function LikePage() {
     console.log("Add to matches")
   }
 
-  function getNewPet() {
-    // Get a pet that matches settings
+  //TODO
+  /*
+  function fillListWithPets() {
 
-    //tmp
-    const img = imgs[Math.floor(Math.random() * 4)]
-    return {petName: "Pet Name", petImg: img, petDetails: "Details about pet"}
+    let p = 10;
+    while(p < 15) {
+      const animals = await fetchAnimals({type:"Dog", page:p})
+      let animalsWithPhotos = animals.animals.filter((animal) => animal.photos.length !== 0)
+      setPetImgs([...petImgs, ...animalsWithPhotos])
+      console.log([...petImgs, ...animalsWithPhotos])
+      p += 1;
+    }
+  }
+  */
+
+  async function getNewPet() {
+    // Only changes photo at the moment 
+    // Fetches a page of animals, filters out those without photos, displays the first one with a photo
+    // Doesnt display others on the page, just goes to next page, needs fix
+    // Doesnt save page
+    let animalsWithPhotos = [];
+    let img;
+    let p = currentPage;
+    while(animalsWithPhotos.length === 0) {
+      const animals = await fetchAnimals({type:petPref, page:p})
+      animalsWithPhotos = animals.animals.filter((animal) => animal.photos.length !== 0)
+      p += 1;
+    }
+    setCurrentPage(p)
+    img = animalsWithPhotos[0].photos[0].large // This might break
+    setPetImg(img)
+    return null
   }
 
   // TODO, make height fit the screen
@@ -46,10 +75,10 @@ function LikePage() {
     <div className="container-sm p-3">
       <h2>Like Pets</h2>
       <Card className="">
-        <Card.Img className="object-fit-scale" variant="top" src={`./tmpimgs/${pet.petImg}`} />
+        <Card.Img className="object-fit-scale" variant="top" src={petImg} />
         <Card.Body>
-          <Card.Title>{pet.petName}</Card.Title>
-          <Card.Text>{pet.petDetails}</Card.Text>
+          <Card.Title>Pet Name</Card.Title>
+          <Card.Text>Pet Details</Card.Text>
           <div className="col">          
             <button type="button" className="btn btn-success btn-lg" onClick={handleLikeClick}>
               Like
