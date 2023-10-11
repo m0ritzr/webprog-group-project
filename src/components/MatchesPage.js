21ca7618e28427e2f8a56c3d03f713b516977ca1
Moritz Rübner <moritz@craft.do>
2023-10-11 20:19:19 +0200

    Update MatchesPage.js

--- src/components/MatchesPage.js
+++ src/components/MatchesPage.js
@@ -1,23 +1,52 @@
-import React from "react";
-import { ListGroup, Card } from "react-bootstrap";
+import { React, useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import { useData } from "../dataContext";
+import { fetchAnimal } from "../petfinder";
 
 function MatchesPage() {
   // eslint-disable-next-line no-unused-vars
-  const { settings, setSettings, matches, setMatches } = useData();
+  const { matches } = useData();
+  const [loadedPetsData, setLoadedPetsData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const loadPets = async () => {
+      const loadedPetsObj = {};
+
+      const promises = matches.map(async match => {
+        if (!loadedPetsObj[match]) {
+          const result = await fetchAnimal(match);
+          loadedPetsObj[match] = result.animal;
+        }
+      });
+
+      await Promise.all(promises);
+
+      console.log(loadedPetsObj);
+      setLoadedPetsData(loadedPetsObj);
+      setIsLoading(false);
+    };
+
+    loadPets();
+
+}, [matches]);
 
   return (
     <div className="p-4">
       <h2>My Matches</h2>
-      <Card className="mt-4">
-        <Card.Body>
-          <ListGroup>
-            <ListGroup.Item>Pet 1: Contact Details</ListGroup.Item>
-            <ListGroup.Item>Pet 2: Contact Details</ListGroup.Item>
-            {/* ... Other matched pets */}
-          </ListGroup>
-        </Card.Body>
-      </Card>
+        {isLoading ? <h3>Loading</h3> : matches.map(match => (
+          <Card className="mt-4 vh-10 mh-20 vw-75" style={{flexDirection: "row"}}>
+            <Card.Img variant="top" className="w-25 mh-100 object-fit-cover" src={loadedPetsData[match].photos[0].medium} />
+            <Card.Body>
+              <Card.Title>{loadedPetsData[match].name}</Card.Title>
+              <Card.Text>
+                {loadedPetsData[match].description}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
     </div>
   );
 }
