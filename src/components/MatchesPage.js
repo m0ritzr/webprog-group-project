886eed163b9dbcdb023b39521c4b93f7562aaceb
annaewerman <eko15aew@student.lu.se>
2023-10-13 10:46:56 +0200

    Update MatchesPage.js

--- src/components/MatchesPage.js
+++ src/components/MatchesPage.js
@@ -1,7 +1,9 @@
 import { React, useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card,  } from "react-bootstrap";
 import { useData } from "../dataContext";
 import { fetchAnimal } from "../petfinder";
+import {Toast} from "bootstrap";
+
 
 function MatchesPage() {
   // eslint-disable-next-line no-unused-vars
@@ -32,6 +34,16 @@ function MatchesPage() {
     loadPets();
   }, [matches]);
 
+  function handleUnmatch(){
+   // setLoadedPetsData( !loadedPetsData[id]);
+    const toastUnmatch = document.getElementById('liveToast');
+    const toast = Toast.getOrCreateInstance(toastUnmatch);
+    toast.show();
+  
+}
+    
+  
+
   return (
     <div className="p-4">
       <h2>My Matches</h2>
@@ -48,6 +60,23 @@ function MatchesPage() {
             <Card.Body>
               <Card.Title>{loadedPetsData[match].name}</Card.Title>
               <Card.Text>{loadedPetsData[match].description}</Card.Text>
+              <Button id="Unmatch" onclick={handleUnmatch} >
+                Unmatch
+              </Button>
+              <div class="toast-container position-fixed bottom-0 end-0 p-3">
+                 <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                  <div class="toast-header">
+                    <strong class="me-auto">Unmatched</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                    </div>
+                    <div class="toast-body">
+                      You have unmatched with {loadedPetsData[match].name}
+                      </div>
+                      </div>
+                      </div>
+            
+
+              
             </Card.Body>
           </Card>
         ))
